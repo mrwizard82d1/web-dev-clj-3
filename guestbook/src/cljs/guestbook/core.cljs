d502ac3b77dad76fbbb229e59d4349573ab8bc46
Larry Jones <mrwizard82d1@gmail.com>
2026-02-25 17:22:23 -0600

    CSS-style shorthand for id and class attributes

--- guestbook/src/cljs/guestbook/core.cljs
+++ guestbook/src/cljs/guestbook/core.cljs
@@ -3,7 +3,7 @@
             [reagent.dom :as dom]))
 
 (dom/render
- ;; Specify tag and attributes **without** shortcuts
- [:div {:id "hello", :class "content"}
+ ;; Use CSS-style shorthand to set id and class attributes
+ [:div#hello.content
   [:h1 "Hello, Reagent World!"]]
  (.getElementById js/document "content"))
