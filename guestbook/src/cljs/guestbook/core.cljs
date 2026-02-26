7ad1c265b4dbb9c7c5af32820849106cd1baa195
Larry Jones <mrwizard82d1@gmail.com>
2026-02-26 13:47:23 -0600

    Capture input (with an r/atom for field values)

--- guestbook/src/cljs/guestbook/core.cljs
+++ guestbook/src/cljs/guestbook/core.cljs
@@ -1,24 +1,47 @@
 ;; React 18+ **does not** support the `render` method; instead, one must use
 ;; `reagent.dom.client/create-root` to create the application root. We create
 ;; this instance only once.
-;;
-;; Note that other solutions initialized the root using an atom initially
-;; defined as `nil`. The state was changed using either `reset!` or `swap!`.
-;; I could not get these solutions to work; I am uncertain why. Sigh...
-;;
-;; But this code works. (Fingers crossed.)
 
 (ns guestbook.core
   (:require [reagent.core :as r]
-            [reagent.dom.client :as rdom]))
+            [reagent.dom.client :as rdomc]))
 
-(defonce root (rdom/create-root (.getElementById js/document "content")))
+(defonce root (rdomc/create-root (.getElementById js/document "content")))
 
-(defn hello-reagent-world []
-  [:div#hello.content
-   [:h1 "Hello, Reagent World!"]])
+(defn message-form []
+  (let [fields (r/atom {})]
+    (fn []
+      [:div
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [:input.input {:type :text
+                       :name :name
+                       :on-change #(swap! fields
+                                          assoc :name (-> % .-target .-value))
+                       :value (:name @fields)}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [:textarea.textarea
+         {:name :message
+          :value (:message @fields)
+          :on-change #(swap! fields
+                             assoc :message (-> %
+                                                .-target .-value))}]]
+       [:input.button.is-primary
+        {:type :submit
+         :value "comment"}]
+       [:div {:style {:margin-top "40px"}}
+        [:strong "Debug"]
+        [:div {:style {:margin-left "10px"}}
+          [:p "Name: " (:name @fields)]
+          [:p "Message: " (:message @fields)]]]])))
+
+(defn home []
+  [:div.content>div.columns.is-centered>div.column.is-two-thirds
+   [:div.columns>div.column
+    [message-form]]])
 
 (defn ^:export start []
-  (rdom/render root [hello-reagent-world]))
+  (rdomc/render root [home]))
 
 (start)
