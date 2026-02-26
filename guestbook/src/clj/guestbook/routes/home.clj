b4d124c1f84ba4d932ac7ea9e564a797b3b4ccff
Larry Jones <mrwizard82d1@gmail.com>
2026-02-26 14:26:54 -0600

    Handle (dummy) valid messages

--- guestbook/src/clj/guestbook/routes/home.clj
+++ guestbook/src/clj/guestbook/routes/home.clj
@@ -24,11 +24,23 @@
 ;
 
 ;
-(defn save-message! [{:keys [params]}]
- (db/save-message! params)
- (response/found "/"))
+(defn validate-message [params]
+  nil
+  ;; {:message ("message is less than the minimum")}
+  )
 ;
 
+;
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (response/bad-request {:errors errors})
+    (try
+      (db/save-message! params)
+      (response/ok {:status :ok})
+      (catch Exception e
+        (response/internal-server-error
+         {:errors {:server-error ["Failed to save message!"]}})))))
+;
 
 (defn about-page [request]
   (layout/render
