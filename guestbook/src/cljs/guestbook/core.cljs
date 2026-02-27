b1537861da9d050d403d12accc8e8f54626adff3
Larry Jones <mrwizard82d1@gmail.com>
2026-02-26 18:41:15 -0600

    Update code to display errors on the page

--- guestbook/src/cljs/guestbook/core.cljs
+++ guestbook/src/cljs/guestbook/core.cljs
@@ -5,48 +5,60 @@
 (ns guestbook.core
   (:require [reagent.core :as r]
             [reagent.dom.client :as rdomc]
-            [ajax.core :refer [GET POST]]))
+            [ajax.core :refer [GET POST]]
+            [clojure.string :as c-str]))
 
 (defonce root (rdomc/create-root (.getElementById js/document "content")))
 
-(defn send-message! [fields]
+(defn send-message! [fields errors]
   ;; Add our anti-forgery token using an `x-csrf-token` header on our request
   (POST "/message"
         {:format :json
          :headers {"Accept" "application/transit+json"
                    "x-csrf-token" (.-value (.getElementById js/document "token"))}
          :params @fields
-         :handler #(.log js/console (str "response: " %))
-         :error-handler #(.error js/console (str "error: " %))}))
+         :handler (fn [r]
+                    (.log js/console (str "response: " r))
+                    (reset! errors nil))
+         :error-handler (fn [e]
+                          (.log js/console (str e))
+                          (reset! errors (-> e
+                                            :response
+                                            :errors)))}))
+
+(defn errors-component [errors id]
+  (when-let [error (id @errors)]
+    [:div.notification.is-danger (c-str/join error)]))
 
 (defn message-form []
-  (let [fields (r/atom {})]
+  (let [fields (r/atom {})
+        errors (r/atom nil)]
     (fn []
       [:div
+       [:p "Name: " (:name @fields)]
+       [:p "Message: " (:message @fields)]
+       [errors-component errors :server-error]
        [:div.field
         [:label.label {:for :name} "Name"]
-        [:input.input {:type :text
-                       :name :name
-                       :on-change #(swap! fields
-                                          assoc :name (-> % .-target .-value))
-                       :value (:name @fields)}]]
+        [errors-component errors :name]
+        [:input.input
+         {:type :text
+          :name :name
+          :on-change #(swap! fields
+                             assoc :name (-> % .-target .-value))
+          :value (:name @fields)}]]
        [:div.field
         [:label.label {:for :message} "Message"]
+        [errors-component errors :message]
         [:textarea.textarea
-         {:name :message
-          :value (:message @fields)
+         {:name      :message
+          :value     (:message @fields)
           :on-change #(swap! fields
-                             assoc :message (-> %
-                                                .-target .-value))}]]
+                             assoc :message (-> % .-target .-value))}]]
        [:input.button.is-primary
-        {:type :submit
-         ::on-click #(send-message! fields)
-         :value "comment"}]
-       [:div {:style {:margin-top "40px"}}
-        [:strong "Debug"]
-        [:div {:style {:margin-left "10px"}}
-          [:p "Name: " (:name @fields)]
-          [:p "Message: " (:message @fields)]]]])))
+        {:type      :submit
+         ::on-click #(send-message! fields errors)
+         :value     "comment"}]])))
 
 (defn home []
   [:div.content>div.columns.is-centered>div.column.is-two-thirds
